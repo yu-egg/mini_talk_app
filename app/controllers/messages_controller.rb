756882c5179406bf8545fd21a244ae2a43aee76d
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-10-09 13:58:36 +0900

    messages_controller.rbを編集

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,11 @@
 class MessagesController < ApplicationController
   def new
+    @messages = Message.all
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(text: params[:message][:text])
   end
 end
+#new.html.erbで保存されているメッセージをすべて表示させたいので、3行目で取得しています。
