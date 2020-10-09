c6bb696a5a4a4f91e3cdba14bd5ecfa798cb109c
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-10-09 14:06:19 +0900

    stream_from stream_fromとは、サーバーとクライアントを関連付けるメソッドです。Action Cableにあらかじめ用意されています。  まずは、message_channel.rbに以下を追記しましょう

--- app/channels/message_channel.rb
+++ app/channels/message_channel.rb
@@ -1,6 +1,7 @@
 class MessageChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "message_channel"
+    # stream_from "some_channel"stream_fromとは、サーバーとクライアントを関連付けるメソッドです。Action Cableにあらかじめ用意されています。
   end
 
   def unsubscribed
