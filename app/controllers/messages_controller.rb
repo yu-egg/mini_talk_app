8b80652abb7582285a2a51330a9bf3b67694b84b
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-10-09 14:06:35 +0900

    broadcastとは、サーバーから送られるデータの経路のことを指します。 broadcastを介してデータをクライアントに送信します。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,6 +6,8 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(text: params[:message][:text])
-  end
+    if @message.save
+      ActionCable.server.broadcast 'message_channel', content: @message
+    end #broadcastとは、サーバーから送られるデータの経路のことを指します。broadcastを介してデータをクライアントに送信します。追記した10行目は、broadcastを通して、'message_channel'に向けて@messageを送信するということです。
 end
 #new.html.erbで保存されているメッセージをすべて表示させたいので、3行目で取得しています。
